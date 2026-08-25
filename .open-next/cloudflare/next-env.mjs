c7bc35843e67c5470ead133ec0d687e9377cb863
babacagdas <koroglucagdas44@gmail.com>
2026-08-25 20:33:02 +0300

    fix(build): Next.js standart temiz build yapisi tamamlandi

---
+++ .open-next/cloudflare/next-env.mjs
@@ -0,0 +1,3 @@
+export const production = {"NEXT_PUBLIC_TMDB_API_KEY":"d0e8d91e3b9f6091fc38629f10e2e049","NEXT_PUBLIC_SUPABASE_URL":"https://rwvkmucurvdkrxxkuukt.supabase.co","NEXT_PUBLIC_SUPABASE_ANON_KEY":"sb_publishable_P-u3qyutmfk9ITFd30SGEA_BrB68_Rn"};
+export const development = {"NEXT_PUBLIC_TMDB_API_KEY":"d0e8d91e3b9f6091fc38629f10e2e049","NEXT_PUBLIC_SUPABASE_URL":"https://rwvkmucurvdkrxxkuukt.supabase.co","NEXT_PUBLIC_SUPABASE_ANON_KEY":"sb_publishable_P-u3qyutmfk9ITFd30SGEA_BrB68_Rn"};
+export const test = {};
